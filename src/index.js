883e3b0d0a41f13563a5fd543e3d1b7c459eeda7
MariiaHula <mariyagulaya@gmail.com>
2023-09-15 19:19:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,15 +12,3 @@ import './js/popular-recipes';
 import { goitGlobalAPI } from './js/axios_api';
 import axiosInstance from "axios";
 
-// if (window.screen > такийто розмір екрану)
-// {
-//     const test = new goitGlobalAPI(6);
-//     }
-    const test = new goitGlobalAPI(6);
-
-test.getEvents().then(response => console.log(response)).catch(err => console.log(err));
-test.getCategories().then(response => console.log(response)).catch(err => console.log(err));
-test.getAreas().then(response => console.log(response)).catch(err => console.log(err));
-test.getPopularRecipes().then(response => console.log(response)).catch(err => console.log(err));
-test.getIgridients().then(response => console.log(response)).catch(err => console.log(err));
-test.getAllRecipesCategories().then(response => console.log(response)).catch(err => console.log(err));
