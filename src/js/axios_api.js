b9f886b1888a8c086e68e7b7d4914acc9dc08426
MariiaHula <mariyagulaya@gmail.com>
2023-09-15 15:13:01 +0300

    changed index.html and favorites.html

---
+++ src/js/axios_api.js
@@ -0,0 +1,40 @@
+import axiosInstance from "axios";
+
+
+
+
+
+
+
+
+// const instanse = axios.create({
+//     baseURL: 'https://pixabay.com/api/',
+//     params: {
+//                 key: '39403655-1e18babd7f788259efcd023e3',
+//                 image_type: 'photo',
+//                 orientation: 'horizontal',
+//                 safesearch: 'true',       
+//             }
+// })
+
+// export class PixabayAPI {
+//     constructor() {
+//         this.q = null;
+//         this.page = 1;
+//         this.per_page = 40;
+//     }
+//     getGalleryCard() {
+//         const options = {
+//             params: {
+//                 q: this.q,
+//                 page: this.page,
+//                 per_page: this.per_page,
+//             }
+//         };
+
+//       return  instanse.get('', options).then(response => response.data);
+//     }
+// }
+
+
+
