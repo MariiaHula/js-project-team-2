917cc9955cc75eff92f4d96187b6cf1a26e7617a
MariiaHula <mariyagulaya@gmail.com>
2023-09-16 22:28:52 +0300

    added js css and html

---
+++ src/js/popular-recipes.js
@@ -0,0 +1,45 @@
+
+import { goitGlobalAPI } from "./axios_api"
+
+
+function markupPopularRecipes(recipesArr) {
+
+    const markup = recipesArr.map(recipe => {
+
+        return `<li class="popular-recipes-item">
+        <a class="popular-recipes-link link" href="${recipe.id}">
+            <img class="img-wrapper" src="${recipe.preview}" alt="${recipe.title}" />
+            <div class="popular-wrapper">
+            <h2 class="recipes-title">${recipe.title}</h2>
+            <p class="recipes-text">${recipe.description}</p>
+            </div>
+          </a>
+      </li>`
+    }).join('');
+
+    return markup;
+}
+ 
+const popularListEl = document.querySelector('.js-popular-list');
+
+const popularRecipes = new goitGlobalAPI();
+
+async function addPopularRecipes() {
+    
+    try {        
+        const data = await popularRecipes.getPopularRecipes();
+        popularListEl.innerHTML = markupPopularRecipes(data);
+                
+        // if (window.innerWidth > 768) {
+        //     popularListEl.innerHTML = markupPopularRecipes(data);
+            
+        //     return;    
+        // } 
+            
+    }
+    catch (err) {
+        console.log(`Error: ${err}`);
+    }
+    
+}
+addPopularRecipes();
