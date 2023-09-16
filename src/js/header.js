0dc87ddb4956dd3dc420f43fc62f87c6959eb3a9
azazaikens <rubanjuri@gmail.com>
2023-09-16 14:23:04 +0300

    socket_header

---
+++ src/js/header.js
@@ -0,0 +1,18 @@
+const myButton = document.querySelector('.header-darck-night');
+
+myButton.addEventListener('click', function () {
+    // Додаємо клас "clicked" при кліку
+myButton.disabled = true;
+  myButton.classList.add('clicked');
+
+  // Тут ви можете додати логіку для обробки кліку
+  // ...
+
+  // Через певний час (наприклад, 500 мілісекунд), видаляємо клас "clicked"
+});
+
+const body = document.querySelector('body')
+
+body.innerHTML = '<p>sofkpsodffk</p>'
+
+console.log(12)
