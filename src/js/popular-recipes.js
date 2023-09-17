5e5d7f26ee37c161b52be17179b7085778263555
MariiaHula <mariyagulaya@gmail.com>
2023-09-17 20:06:45 +0300

    update popular-recipes.js

--- src/js/popular-recipes.js
+++ src/js/popular-recipes.js
@@ -1,11 +1,7 @@
 
 import { goitGlobalAPI } from "./axios_api"
-
-
 function markupPopularRecipes(recipesArr) {
-
     const markup = recipesArr.map(recipe => {
-
         return `<li class="popular-recipes-item">
         <a class="popular-recipes-link link js-popular" href="#">
             <img class="popular-img-wrapper" src="${recipe.preview}" alt="${recipe.title}" />
@@ -16,81 +12,60 @@ function markupPopularRecipes(recipesArr) {
           </a>
       </li>`
     }).join('');
-
     return markup;
 }
- 
+
 const popularListEl = document.querySelector('.js-popular-list');
 
 const popularRecipes = new goitGlobalAPI();
 
 function truncateTextMobile(data) {
-
-    if (data.title.length > 17) {
-      return  data.title.substring(0, 18) + '...';
- }
-    if (data.description.length > 75) {
-     return  data.description.substring(0, 75) + '...';
- }
- 
-
-    // let truncatedTitle = title.substring(0, 18) + "...";
-    // let truncatedText = text.substring(0, 75) + "...";
-    // document.querySelector('.js-title').innerHTML = truncatedTitle;
-    // document.querySelector('.js-text').innerHTML = truncatedText;
-   
+  if (data.title.length > 17) {
+    data.title = data.title.substring(0, 18) + '...';
+  }
+  if (data.description.length > 75) {
+    data.description = data.description.substring(0, 75) + '...';
+  }
 }
+// function truncateTablet() {
 
-function truncateTablet() {
-    let title = document.querySelector('.js-title').innerHTML;
-    let text = document.querySelector('.js-text').innerHTML;
-    let truncatedTitle = title.substring(0, 13) + "...";
-    let truncatedText = text.substring(0, 70) + "...";
-    document.querySelector('.js-title').innerHTML = truncatedTitle;
-    document.querySelector('.js-text').innerHTML = truncatedText;
-}
-function truncateDesctop() {
-    let title = document.querySelector('.js-title').innerHTML;
-    let text = document.querySelector('.js-text').innerHTML;
-    let truncatedTitle = title.substring(0, 13) + "...";
-    let truncatedText = text.substring(0, 70) + "...";
-    document.querySelector('.js-title').innerHTML = truncatedTitle;
-    document.querySelector('.js-text').innerHTML = truncatedText;
-}
+// }
+// function truncateDesctop() {
 
- 
+// }
 async function addPopularRecipes() {
-    
-    try {        
+    try {
         const data = await popularRecipes.getPopularRecipes();
         console.log(data);
-                
         if (window.innerWidth < 768) {
-            
             let string = '';
-
-            for (let i = 0; i <= data.length/2; i++) {
-                string = markupPopularRecipes([data[i]]);
-                truncateTextMobile(data[i]); 
-                console.log(1);
+            for (let i = 0; i < (data.length - 1) / 2; i++) {
+                truncateTextMobile(data[i]);
+                string += markupPopularRecipes([data[i]]);
+                console.log(data[i]);
             }
-
             popularListEl.innerHTML = string;
-            // popularListEl.innerHTML = markupPopularRecipes([data[0], data[1]]);
-            // truncateTextMobile();   
             
         } else if (window.innerWidth > 768 && window.innerWidth < 1280) {
-            popularListEl.innerHTML = markupPopularRecipes(data);
-            truncateTablet();
+            let string = '';
+            for (let i = 0; i < data.length; i++) {
+              truncateTextMobile(data[i]);
+              string += markupPopularRecipes([data[i]]);
+              console.log(data[i]);
+            }
+            popularListEl.innerHTML = string;
         } else {
-            popularListEl.innerHTML = markupPopularRecipes(data);
-            truncateDesctop();
+            let string = '';
+            for (let i = 0; i < data.length; i++) {
+              truncateTextMobile(data[i]);
+              string += markupPopularRecipes([data[i]]);
+              console.log(data[i]);
+            }
+            popularListEl.innerHTML = string;
         }
-            
     }
     catch (err) {
         console.log(`Error: ${err}`);
     }
-    
 }
 addPopularRecipes();
