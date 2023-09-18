1b357731a740daa227165ca216b734c63613a70f
MariiaHula <mariyagulaya@gmail.com>
2023-09-18 09:36:55 +0300

    added
    added scroll-behavior: smooth;
    update popular-recipes.css,  popular-recipes.css,

--- src/js/popular-recipes.js
+++ src/js/popular-recipes.js
@@ -19,49 +19,14 @@ const popularListEl = document.querySelector('.js-popular-list');
 
 const popularRecipes = new goitGlobalAPI();
 
-function truncateTextMobile(data) {
-  if (data.title.length > 17) {
-    data.title = data.title.substring(0, 18) + '...';
-  }
-  if (data.description.length > 75) {
-    data.description = data.description.substring(0, 75) + '...';
-  }
-}
-// function truncateTablet() {
-
-// }
-// function truncateDesctop() {
-
-// }
 async function addPopularRecipes() {
     try {
         const data = await popularRecipes.getPopularRecipes();
-        console.log(data);
+
         if (window.innerWidth < 768) {
-            let string = '';
-            for (let i = 0; i < (data.length - 1) / 2; i++) {
-                truncateTextMobile(data[i]);
-                string += markupPopularRecipes([data[i]]);
-                console.log(data[i]);
-            }
-            popularListEl.innerHTML = string;
-            
-        } else if (window.innerWidth > 768 && window.innerWidth < 1280) {
-            let string = '';
-            for (let i = 0; i < data.length; i++) {
-              truncateTextMobile(data[i]);
-              string += markupPopularRecipes([data[i]]);
-              console.log(data[i]);
-            }
-            popularListEl.innerHTML = string;
+            popularListEl.innerHTML = markupPopularRecipes([data[0], data[1]]);            
         } else {
-            let string = '';
-            for (let i = 0; i < data.length; i++) {
-              truncateTextMobile(data[i]);
-              string += markupPopularRecipes([data[i]]);
-              console.log(data[i]);
-            }
-            popularListEl.innerHTML = string;
+            popularListEl.innerHTML = markupPopularRecipes(data);     
         }
     }
     catch (err) {
