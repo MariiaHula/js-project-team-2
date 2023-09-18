e17c8ecd2751696d755b3577cb70719cfdc51e49
MariiaHula <mariyagulaya@gmail.com>
2023-09-18 11:54:23 +0300

    update section popular
    added icon in the sprite, added styles, updated popular-recipes.js

--- src/js/popular-recipes.js
+++ src/js/popular-recipes.js
@@ -3,11 +3,11 @@ import { goitGlobalAPI } from "./axios_api"
 function markupPopularRecipes(recipesArr) {
     const markup = recipesArr.map(recipe => {
         return `<li class="popular-recipes-item">
-        <a class="popular-recipes-link link js-popular" href="#">
+        <a class="link popular-recipes-link js-popular" href="#" id="${recipe._id}">
             <img class="popular-img-wrapper" src="${recipe.preview}" alt="${recipe.title}" />
             <div class="popular-wrapper">
-            <h2 class="popular-recipes-head js-title">${recipe.title}</h2>
-            <p class="popular-recipes-text js-text">${recipe.description}</p>
+            <h2 class="popular-recipes-head">${recipe.title}</h2>
+            <p class="popular-recipes-text">${recipe.description}</p>
             </div>
           </a>
       </li>`
@@ -24,7 +24,8 @@ async function addPopularRecipes() {
         const data = await popularRecipes.getPopularRecipes();
 
         if (window.innerWidth < 768) {
-            popularListEl.innerHTML = markupPopularRecipes([data[0], data[1]]);            
+            popularListEl.innerHTML = markupPopularRecipes([data[0], data[1]]);  
+            console.log(data);
         } else {
             popularListEl.innerHTML = markupPopularRecipes(data);     
         }
@@ -34,3 +35,4 @@ async function addPopularRecipes() {
     }
 }
 addPopularRecipes();
+
