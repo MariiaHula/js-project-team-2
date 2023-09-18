5c7b2b44650358203f2452a51731704a0cd75da4
azazaikens <rubanjuri@gmail.com>
2023-09-18 11:51:01 +0300

    create_mobail_modal

--- src/js/header.js
+++ src/js/header.js
@@ -1,29 +1,42 @@
-import debounce from "lodash.debounce";
+import debounce from 'lodash.debounce';
 
 const refsHeader = {
-    navigationMenuLink: document.querySelectorAll('.header-nav-menu-a'),
-    burger: document.querySelector('.header-bth-burger'),
-    currentURL: window.location.href,
-}
+  navigationMenuLink: document.querySelectorAll('.header-nav-menu-a'),
+  mobailModal: document.querySelector('.header-burger-modal-window'),
+  closeMobailMenu: document.querySelector('.header-bth-modal-close'),
+  burger: document.querySelector('.header-bth-burger'),
+  currentURL: window.location.href,
+};
+console.log('xep')
+// if (visualViewport < 767) {
+//     refsHeader.burger.classList.remove('is-hidden');
+//     console.log('aefad')
+// }
+// window.addEventListener('resize', debounce(() => {
+//     let visualViewport = window.innerWidth;
 
-window.addEventListener('resize', debounce(() => {
-    let visualViewport = window.innerWidth;
-        if (visualViewport < 767) {
-            refsHeader.burger.classList.remove('is-hidden')
-        } else {
-            refsHeader.burger.classList.add('is-hidden')
-    }
-    // console.log(1)
-    }, 250)
+//     if (visualViewport < 767) {
+//         refsHeader.burger.classList.remove('is-hidden')
+//     } else {
+//         refsHeader.burger.classList.add('is-hidden')
+//     }
+//     // console.log(1)
+// }, 250)
+// );
+
+refsHeader.burger.addEventListener('click', () =>
+  refsHeader.mobailModal.classList.remove('is-hidden')
+);
+refsHeader.closeMobailMenu.addEventListener('click', () =>
+  refsHeader.mobailModal.classList.add('is-hidden')
 );
 
-refsHeader.navigationMenuLink.forEach((link) => {
+refsHeader.navigationMenuLink.forEach(link => {
   if (link.href === refsHeader.currentURL) {
-      link.classList.add('is-active');
-        //   console.log(12);
-
+    link.classList.add('is-active');
+    //   console.log(12);
   } else {
-      link.classList.remove('is-active')
+    link.classList.remove('is-active');
     //   console.log(refsHeader.currentURL);
     //   console.log(link.href);
   }
