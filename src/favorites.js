63a48ea19ebaf67e028c88de32cf1ef256281be6
azazaikens <rubanjuri@gmail.com>
2023-09-20 22:06:57 +0300

    +

--- src/favorites.js
+++ src/favorites.js
@@ -8,10 +8,10 @@ import './js/modal-rating';
 import './js/modal-recipes';
 import './js/popular-recipes';
 import { markupGalleryCard } from './js/render-gallery';
+import { goitGlobalAPI } from './js/axios_api';
 const refsFav = {
-    cardList: document.querySelector('.favorites-list'),
+  cardList: document.querySelector('.favorites-list'),
 };
-
 
 const clickHeart = document.querySelector('.gallery-list');
 let arrGalleryItem = [];
@@ -53,4 +53,56 @@ clickHeart.addEventListener('click', ev => {
   }
 });
 
-console.log(123)
+let favoritRenderAPI;
+
+if (window.innerWidth < 768) {
+  favoritRenderAPI = new goitGlobalAPI(6);
+} else if (window.innerWidth > 768 && window.innerWidth < 1280) {
+  favoritRenderAPI = new goitGlobalAPI(8);
+} else {
+  favoritRenderAPI = new goitGlobalAPI(9);
+}
+
+async function cardFavoriteRender() {
+    favoritRenderAPI.page = 1;
+    try {
+        const response = await favoritRenderAPI.getRecipes();
+        console.log(response)
+        const localArray = window.localStorage.getItem('galleryItem').split(',');
+        const xz = localArray.map(id => {
+            return response.results.map(el => {
+                if (id === el.id) {
+                    refsFav.cardList.innerHTML = markupGalleryCard(el);
+                }
+            })
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//     const options = {
+//       totalItems: response.results.length * response.totalPages,
+//       itemsPerPage: favoritRenderAPI.perPage,
+//       visiblePages: 3,
+//       page: favoritRenderAPI.page,
+//     };
+
+//     const pagination = new Pagination('pagination', options);
+
+//     pagination.on('afterMove', async event => {
+//       favoritRenderAPI.page = event.page;
+//       try {
+//         const response = await favoritRenderAPI.getRecipes();
+//         refs.galleryListEl.innerHTML = markupGalleryCard(response.results);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+cardFavoriteRender()
+
+console.log(1223);
