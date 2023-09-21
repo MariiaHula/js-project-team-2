ae009dfffcc08ece24f1c1c8a930cb293ce42d14
MariiaHula <mariyagulaya@gmail.com>
2023-09-21 11:59:00 +0300

    Revert "Merge pull request #88 from MariiaHula/gallery11"
    This reverts commit 83e16678179b5c5fb793d0f565b2eb48c11b9339, reversing
    changes made to 35988ddbf12f0dac26099d35c157487a528f6ab7.

--- src/js/render-gallery.js
+++ src/js/render-gallery.js
@@ -3,20 +3,10 @@ import localStorage from './localStorage';
 export function markupGalleryCard(arr) {
 
   if (!Array.isArray(arr)) {
+
     return '';
   }
-
   const markup = arr.map(cardEl => {
-    let ratingStars = '';
-    console.log(arr)
-    for (let i = 1; i <= 5; i++) {
-      ratingStars += `
-        <input type="radio" id="star${i}" name="rate" value="${i}" checked />
-        <label for="star${i}" title="${i} star">${i} star</label>
-      `;
-    }
-
-
     return `
        <li class="gallery-item" data-category="${cardEl.category}" >
           <label class="label" >
@@ -43,11 +33,19 @@ export function markupGalleryCard(arr) {
           <h2 class="gallery-card-title">${cardEl.title}</h2>
           <h3 class="gallery-card-subtitle">${cardEl.description}</h3>
           <form class="gallery-form-rating">
-          <label class="gallery-average-rating">${cardEl.rating}</label>
            <div class="rate">
-               ${ratingStars}
+              <input type="radio" id="star5" name="rate" value="5" />
+              <label for="star5" title="text">5 stars</label>
+              <input type="radio" id="star4" name="rate" value="4" />
+              <label for="star4" title="text">4 stars</label>
+              <input type="radio" id="star3" name="rate" value="3" />
+              <label for="star3" title="text">3 stars</label>
+              <input type="radio" id="star2" name="rate" value="2" />
+              <label for="star2" title="text">2 stars</label>
+              <input type="radio" id="star1" name="rate" value="1" />
+              <label for="star1" title="text">1 star</label>
             </div>
-
+            
 
             <button type="button" class="gallery-btn" data-id="${cardEl._id}">See recipe</button>
           </form>
@@ -56,7 +54,7 @@ export function markupGalleryCard(arr) {
     `;
 
   }).join('');
-
+  
   return markup;
 
 }
@@ -89,5 +87,7 @@ export function checkFavorites() {
         localStorage.remove('favorites-recipes', String(event.target.dataset['id']));
       }
     });
+  }
 
+}
 
