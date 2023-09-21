5948b4278d8857d0667427b136da53139b98095a
MariiaHula <138207508+MariiaHula@users.noreply.github.com>
2023-09-21 09:17:56 +0000

    Deploying to gh-pages from @ MariiaHula/js-project-team-2@01fb264d3b9e10ebf290d2300b18e9183d1f2a1b 🚀

--- favorites.aaddc3f7.js
+++
@@ -1,2 +0,0 @@
-var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},n={},o={},r=e.parcelRequired7c6;null==r&&((r=function(e){if(e in n)return n[e].exports;if(e in o){var r=o[e];delete o[e];var i={id:e,exports:{}};return n[e]=i,r.call(i.exports,i,i.exports),i.exports}var d=new Error("Cannot find module '"+e+"'");throw d.code="MODULE_NOT_FOUND",d}).register=function(e,n){o[e]=n},e.parcelRequired7c6=r),r("da7oM"),r("bUb57"),r("31u3U");
-//# sourceMappingURL=favorites.aaddc3f7.js.map
