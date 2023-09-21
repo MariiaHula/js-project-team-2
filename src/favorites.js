4513c02f9ad514124b622a3cc4805685636d4779
MariiaHula <mariyagulaya@gmail.com>
2023-09-21 17:29:02 +0300

    Update favorites.js

--- src/favorites.js
+++ src/favorites.js
@@ -2,8 +2,8 @@ import './js/axios_api';
 import './js/header';
 import localStorage from './js/localStorage';
 import { goitGlobalAPI } from './js/axios_api';
-import { checkFavorites } from './render-gallery';
-import { renderAllRecipes } from './categories';
+import { checkFavorites } from './js/render-gallery';
+import { renderAllRecipes } from './js/categories';
 import Pagination from 'tui-pagination';
 import '../node_modules/tui-pagination/dist/tui-pagination.css';  
 
